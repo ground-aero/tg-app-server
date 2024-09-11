2bb63175583e026b2ba7f69a707d43ebed162f27
unknown <ea.avdeev@gmail.com>
2024-09-11 21:23:22 +0300

    refactor(server): improve

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# tg-app
-
 # Part-2 (Server). Telegram Mini App 'Three-in-One'
+
+### Name of the App if Telegram: @TgGroundBot
 
 ### Backend deployed to '' server:
 Server link: [https://.../](https://.../)
--- server.js
+++ server.js
@@ -18,9 +18,6 @@ const bot = new TelegramBot(JWT_SECRET, { polling: true });
 const WEATHER_API_KEY = 'd3db6cbce06f4567aa0162457240709';
 const webAppUrl = 'https://tg-app-client.netlify.app'
 
-// Serve static files
-// app.use(express.static('public'));
-
 // Хендлер соединения WebSocket
 wss.on('connection', (ws) => {
   console.log('New WebSocket connection');
@@ -39,23 +36,23 @@ wss.on('connection', (ws) => {
   });
 });
 
-
 const menuOptions = {
   reply_markup: JSON.stringify({
     inline_keyboard: [
-      [{text: 'Инфо', callback_data: '/info'}],
       [{text: 'Чат', callback_data: '/chat'}],
-      [{text: 'Погода', callback_data: '/forecast'}],
-      [{text: 'Назад', callback_data: '/back'}],
+      [{text: 'Погода', callback_data: '/weather'}],
+      [{text: 'Прогноз', callback_data: '/forecast'}],
+      [{text: 'Инфо', callback_data: '/info'}],
     ]
   })
 }
 
 bot.setMyCommands([
-  {command:'/start', description: `Подключение к App 'TgGroundBot'`},
-  {command:'/info', description: `Получить инфо о пользователе.`},
+  // {command:'/start', description: `Подключение к App 'TgGroundBot'`},
   {command:'/chat', description: `Открыть чат`},
-  {command:'/forecast', description: `Получить инфо о погоде.`},
+  {command:'/weather', description: `Инфо о погоде`},
+  {command:'/forecast', description: `Прогноз погоды`},
+  {command:'/info', description: `Инфо о пользователе`},
 ])
 
 
@@ -69,30 +66,49 @@ const start = async () => {
     const text = msg.text;
       console.log(msg) // ПОЛУЧАЕМ сообщ из ТГ
   
+    // МЕНЮ В ПАНЕЛИ ТГ
+    // if (text === '/start') {
+    //   return bot.sendMessage(chatId, `Добро пожаловать в чат TgGroundBot. Выберите пункт меню ниже в панели:`, menuOptions);
+    // }
     if (text === '/start') {
-      return bot.sendMessage(chatId, `Добро пожаловать в чат TgGroundBot. Выберите пункт меню ниже`, menuOptions);
+      return bot.sendMessage(chatId, `Добро пожаловать в чат TgGroundBot. Выберите пункт меню ниже:`, {
+        reply_markup: {
+          inline_keyboard: [
+            [{text: 'Открыть окно', web_app: {url: webAppUrl}} ],
+          ]}
+        });
     }
-    if (text === '/info') {
-      return bot.sendMessage(chatId, `${msg.from.first_name} ${msg.from.last_name}`, menuOptions);
-    }
+
     if (text === '/chat') {
-      return bot.sendMessage(chatId, `Здесь чат`, {
+      return bot.sendMessage(chatId, `Открыть чат`, {
         reply_markup: {
           inline_keyboard: [
             [{text: 'Чат', web_app: {url: webAppUrl}} ],
           ]}
         });
     }
-    if (text === '/forecast') {
-      return bot.sendMessage(chatId, `Здесь будет инфо о погоде`, {
+    if (text === '/weather') {
+      return bot.sendMessage(chatId, `Открыть погоду`, {
         reply_markup: {
           inline_keyboard: [
             [{text: 'Погода0', web_app: {url: webAppUrl }} ],
           ]}
         });
     }
+    if (text === '/forecast') {
+      return bot.sendMessage(chatId, `Открыть прогноз`, {
+        reply_markup: {
+          inline_keyboard: [
+            [{text: 'Прогноз', web_app: {url: webAppUrl }} ],
+          ]}
+        });
+    }
+
+    if (text === '/info') {
+      return bot.sendMessage(chatId, `${msg.from.first_name} ${msg.from.last_name}`, menuOptions);
+    }
   
-    // // send a message to the chat acknowledging receipt of their message
+    // отправляем в чат уведомление о получении их сообщения
     return bot.sendMessage(chatId, `Я вас не понял, попробуйте еще раз!`); // ОТПРАВЛЯЕМ СООБЩ В ТЕЛЕГУ
   });
 
@@ -139,5 +155,5 @@ const PORT = 4000
 server.listen(PORT, () => { console.log(`Server is running on port ${PORT}`) });
 
 // server.listen(8000, () => {
-//   console.log('Server is running on port 4000'); //3000
+//   console.log('Server is running on port 8000'); //8000
 // });
